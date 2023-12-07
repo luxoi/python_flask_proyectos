bec11272eb626bdceae69b3b3195ba2821059813
Luis Loyola Cuevas <150284189+luxoi@users.noreply.github.com>
2023-12-06 23:03:08 -0300

    Add files via upload

---
+++ CreateReadusers/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+from users import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    users = User.muestra_usuarios()
+    return render_template("index.html",users=users) 
+
+@app.route('/new')
+def new():
+    return render_template("new.html")
+
+@app.route('/create', methods=['POST'])
+def create():
+    print(request.form)
+    User.guardar(request.form)
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
